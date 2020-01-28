8d9ce9e50a3003271b35d0ec7f0cd37e8d091bec
ignaspa <ignaspan@gmail.com>
2020-01-28 12:00:15 -0500

    success getting mail to create

--- spoilerinator/app/mailers/spoiler_mailer.rb
+++ spoilerinator/app/mailers/spoiler_mailer.rb
@@ -1,8 +1,10 @@
 class SpoilerMailer < ApplicationMailer
     default from: 'ignaspan@gmail.com'
-  def new_spoiler_email
-    @spoiler = params[:spoiler]
-
-    mail(to: @spoiler.email, subject: @spoiler.emailtitle)
+  def new_spoiler_email(spoiler)
+    @spoiler = spoiler
+    puts "*************************"
+    puts @spoiler
+    puts "*************************"
+    mail(:to => @spoiler.email, :subject => @spoiler.emailtitle)
   end
 end
--- spoilerinator/test/mailers/previews/spoiler_mailer_preview.rb
+++ spoilerinator/test/mailers/previews/spoiler_mailer_preview.rb
@@ -4,6 +4,6 @@ class SpoilerMailerPreview < ActionMailer::Preview
         # Set up a temporary order for the preview
         spoiler = Spoiler.new(email: "carvedtriangle@gmail.com", mailtitle: "Ignas test", spoilercontent: "Yeet Skeet!")
     
-        SpoilerMailer.with(spoiler: spoiler).new_spoiler_email
+        SpoilerMailer.new_spoiler_email(spoiler: spoiler)
       end
 end

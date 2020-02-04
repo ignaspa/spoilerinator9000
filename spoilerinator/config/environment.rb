105ce6f4c90d40972afc3abb325e5e92918bff97
ignaspa <ignaspan@gmail.com>
2020-02-04 13:05:43 -0500

    where to put email

--- spoilerinator/config/environment.rb
+++ spoilerinator/config/environment.rb
@@ -3,15 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# config.action_mailer.delivery_method = :smtp
-# config.action_mailer.smtp_settings = {
-#     address:              'smtp.gmail.com',
-#     port:                 587,
-#     domain:               'example.com',
-#     user_name:            'ignaspan@gmail.com',
-#     password:             '<pswd>',
-#     authentication:       'plain',
-#     enable_starttls_auto: true  
-# }
-#  ActionMailer::Base.default_content_type = "text/html"
--- spoilerinator/config/environments/development.rb
+++ spoilerinator/config/environments/development.rb
@@ -40,5 +40,16 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            '<email>',
+    password:             '<password>',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+}
+ 
 
 end
